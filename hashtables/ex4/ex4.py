18f224a68dee91fe462430a2235a5a72cfa12f60
Jose Medrano (Joe) <51244239+JoeLorenzo@users.noreply.github.com>
2020-10-02 11:43:10 -0500

    -ex4 test passing

--- hashtables/ex4/ex4.py
+++ hashtables/ex4/ex4.py
@@ -2,9 +2,39 @@ def has_negatives(a):
     """
     YOUR CODE HERE
     """
-    # Your code here
+     # my approach would be to keep track of negative and positive numbers
+    # in different dictionaries and lastly compare the two dictionary
 
-    return result
+    # dict of positive numbers
+    positive = {}
+    # dict of the absolute values of negative numbers to easily compare the
+    # the two dicts for duplicates.  Making the negative numbers positive now
+    # becomes a simple problem of finding duplicates
+    negative = {}
+    # a dict to push all the common absolute values
+    dups = {}
+    for num in a:
+        if num not in positive or negative:
+            # if positive
+            if num > 0:
+                # numbers will be our keys.  since we don't care about
+                # anything else we will set the value to zero as a default.
+                positive[num] = 0
+            # if negative
+            elif num < 0:
+                # if negative number then get the absolute value of 
+                # the negative number and push it to the negative dictionary 
+                negative[(-1) * num] = 0
+
+    # iterate through one dictionary and compare it to the other
+    # if common values are found then push to the duplicate dict
+    for k in positive.keys():
+        if k in negative:
+            dups[k] = 0
+    
+    # convert keys to a list to satisfy desired output
+    return list(dups.keys())
+
 
 
 if __name__ == "__main__":
