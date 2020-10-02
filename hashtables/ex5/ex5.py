1326aac65b1683bf3513f65ede79ee13ffdce9b9
Jose Medrano (Joe) <51244239+JoeLorenzo@users.noreply.github.com>
2020-10-02 11:26:32 -0500

    -ex5 test passing

--- hashtables/ex5/ex5.py
+++ hashtables/ex5/ex5.py
@@ -6,7 +6,29 @@ def finder(files, queries):
     """
     YOUR CODE HERE
     """
-    # Your code here
+    file_dict = {}
+
+    for file in files:
+        temp = file
+        file_name = ""
+        while temp[-1] != "/":
+            file_name = temp[-1] + file_name
+            temp = temp[:-1]
+        if file_name not in file_dict:
+            # print(f"not in dict adding {file_name}")   
+            file_dict[file_name] = {}
+            file_dict[file_name][file] = 0
+        else:
+            # print(f"already in dict {file_name}")   
+            file_dict[file_name][file] = 0
+        file_name = ""
+    q = []
+    for query in queries:
+        if query in file_dict:
+            for key in file_dict[query].keys():
+                q.append(key)
+    return q
+
 
     return result
 
@@ -15,7 +37,8 @@ if __name__ == "__main__":
     files = [
         '/bin/foo',
         '/bin/bar',
-        '/usr/bin/baz'
+        '/usr/bin/baz',
+        'home/bin/baz'
     ]
     queries = [
         "foo",
